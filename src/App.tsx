0210860f43a4bbca4833c104defef80d234cbbc7
Takanori Hoshi <takanori.hoshi@gmail.com>
2019-05-06 09:01:13 -0700

    Implement simple counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = (): JSX.Element => {
+  const [count, setCount] = useState(0);
   return (
-    <h1>Hello React</h1>
+    <>
+      <h1>Hello React</h1>
+      <div className='count'>{count}</div>
+      <div className='buttons'>
+        <button onClick={() => { setCount(count - 1); return false; }}>-</button>
+        <button onClick={() => { setCount(count + 1); return false; }}>+</button>
+      </div>
+    </>
   );
 };
 
